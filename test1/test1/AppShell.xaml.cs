c6c11ca07da4e060e1b6278309dce17ea7aa2a3a
modabada <miw1102@naver.com>
2022-01-06 09:09:19 +0900

    초기 커밋.

---
+++ test1/test1/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using test1.ViewModels;
+using test1.Views;
+using Xamarin.Forms;
+
+namespace test1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
