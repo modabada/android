494933edfde3a3e34c6ef804de247b50e7e63c5b
modabada <miw1102@naver.com>
2022-01-08 14:08:44 +0900

    Graphic, Button 연습 as, is 키워드 공부

--- MyApp/MyApp/MainPage.xaml.cs
+++ MyApp/MyApp/MainPage.xaml.cs
@@ -8,8 +8,27 @@ using Xamarin.Forms;
 
 namespace MyApp {
     public partial class MainPage: ContentPage {
+        int B_cnt = 0;
         public MainPage() {
             InitializeComponent();
+        }
+
+        public void ButtonEvent(object sender, EventArgs e) {
+            switch(B_cnt) {
+                case 0:
+                    (sender as Button).Text = "1st click!";
+                    break;
+                case 1:
+                    ((Button) (sender)).Text = "2nd click!";
+                    break;
+                case 2:
+                    (sender as Button).Text = (sender is Button) ? "true" : "false";
+                    break;
+                default:
+                    (sender as Button).Text = "Clicked";
+                    break;
+            }
+            B_cnt++;
         }
     }
 }
